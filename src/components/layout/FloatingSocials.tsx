800ddf5a47593f62a4ca8b31ac246cd353136e1f
Alex Gianmarco Yupanqui Ramos <agyupanquira@unitru.edu.pe>
2026-06-22 09:32:21 -0500

    C3

--- src/components/layout/FloatingSocials.tsx
+++ src/components/layout/FloatingSocials.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function FloatingSocials() {
   return (
-    <div className="fixed bottom-6 right-6 z-50 flex flex-col gap-3">
+    <div className="fixed bottom-6 right-6 z-50 flex flex-col gap-3 transform-gpu will-change-transform">
       {/* Facebook */}
       <a
         href="https://facebook.com"
